7a1d0b8232f3bbb49723b4dbd4c919edb4efdb1b
jcborras <juanb@oyster.(none)>
2011-05-23 11:19:04 +0300

    Tests cover chopping now

--- tests/1.R
+++ tests/1.R
@@ -53,9 +53,13 @@ expect_that(file.exists(logfile(logger)), is_true())
 unlink(logfile(logger))
 
 ## verbosity() test cases
+expect_that(verbosity(-19), equals(log4r:::FATAL))
+expect_that(verbosity(-1), equals(log4r:::FATAL))
 expect_that(verbosity(1), equals(log4r:::FATAL))
 expect_that(verbosity(2), equals(log4r:::ERROR))
 expect_that(verbosity(3), equals(log4r:::WARN))
 expect_that(verbosity(4), equals(log4r:::INFO))
 expect_that(verbosity(5), equals(log4r:::DEBUG))
+expect_that(verbosity(6), equals(log4r:::DEBUG))
+expect_that(verbosity(60), equals(log4r:::DEBUG))
 ## A real use case scenario in the .Rd help file
