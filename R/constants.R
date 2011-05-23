fdbab0774128c5055b1298b61efc07506b5d2d3a
jcborras <juanb@oyster.(none)>
2011-05-23 11:12:02 +0300

    Ops! previous commit did not contain actual R code

--- R/constants.R
+++ R/constants.R
@@ -4,3 +4,9 @@ WARN <- 3
 ERROR <- 4
 FATAL <- 5
 LEVELS <- seq(DEBUG, FATAL)
+
+verbosity <- function(i) {
+  i <- ifelse(i>max(LEVELS), max(LEVELS), i)
+  i <- ifelse(i<min(LEVELS), min(LEVELS), i)
+  return(LEVELS[max(LEVELS)-i+1])
+}
