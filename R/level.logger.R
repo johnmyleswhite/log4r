06a08232bcc44fd0898efb49839f05f8873b6c63
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-09-26 00:09:53 +0200

    level.logger operates with objects of class loglevel

--- R/level.logger.R
+++ R/level.logger.R
@@ -1,22 +1,35 @@
-#' Get the priority level for a logger object.
+#' Set or get the priority level for a logger object.
 #'
-#' The priority level can be one of:
-#' \code{log4r:::DEBUG}, \code{log4r:::INFO}, \code{log4r:::WARN},
-#' \code{log4r:::ERROR} or \code{log4r:::FATAL}
+#' The priority level can be an integer from the set 1..5 (otherwise it will be
+#' modified sensibly to fit in that range), or a named logging level
+#' (one of \Sexpr{paste0('"', log4r:::LEVEL_NAMES, '"', collapse = ", ")}).
+#' An object of class loglevel is also accepted; other input will be coerced
+#' using \code{\link{as.loglevel}}.
 #'
-#'
-#' @aliases level.logger level
+#' @aliases level<-.logger level<-
 #' @param x An object of class logger.
+#' @param value A loglevel.
+#' @seealso \code{\link{loglevel}}
 #' @examples
 #'
 #' library('log4r')
 #'
-#' logger <- create.logger(logfile = 'debugging.log', level = log4r:::DEBUG)
-#'
-#' print(level(logger))
-#' @export level.logger
+#' logger <- create.logger(logfile = 'debugging.log', level = 1)
+#' level(logger)
+#' level(logger) <- "FATAL"
+#' @export
 `level.logger` <-
-function(x)
-{
-  return(x[['level']])
-}
+  function(x)
+  {
+    return(x[['level']])
+  }
+
+#' @rdname level.logger
+#' @export
+`level<-.logger` <-
+  function(x, value)
+  {
+    x[['level']] <- as.loglevel(value)
+    return(x)
+  }
+
