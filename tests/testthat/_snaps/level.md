9bfdbb55a820466aac98334935d6e07a034e747b
Aaron Jacobs <atheriel@gmail.com>
2024-10-12 02:28:59 -0400

    Deprecate much of the pre-0.3.0 API.
    This commit does the following:
    
    * Adopts the `lifecycle` package to get consistent, high-quality
      deprecation warnings and errors, plus nice badges for the docs.
    
    * Deprecates the pre-appender & layout APIs.
    
    * Deprecates APIs that expose implementation details of levels, which
      are unlikely to have any external users anyway.
    
    * Soft-deprecates the existing logging functions in favour of ones with
      a more friendly `log_` prefix.
    
    * Renames and reorganises some source and test files.
    
    Closes #29.
    
    Signed-off-by: Aaron Jacobs <atheriel@gmail.com>

---
+++ tests/testthat/_snaps/level.md
@@ -0,0 +1,28 @@
+# The logger threshold works as expected
+
+    Code
+      level(lgr)
+    Output
+      FATAL 
+
+# Levels can be listed
+
+    Code
+      available.loglevels()
+    Output
+      $DEBUG
+      DEBUG 
+      
+      $INFO
+      INFO 
+      
+      $WARN
+      WARN 
+      
+      $ERROR
+      ERROR 
+      
+      $FATAL
+      FATAL 
+      
+
--- tests/testthat/test-level.R
+++ tests/testthat/test-level.R
@@ -1,4 +1,18 @@
+test_that("The logger threshold works as expected", {
+  expect_error(logger(threshold = "UNKNOWN"), "unknown logging level")
+  expect_error(logger(threshold = NULL), "must be a string")
+  lgr <- logger()
+  level(lgr) <- "FATAL"
+  expect_snapshot(level(lgr))
+})
+
+test_that("Levels can be listed", {
+  expect_snapshot(available.loglevels())
+})
+
 test_that("The loglevel() constructor works as expected", {
+  rlang::local_options(lifecycle_verbosity = "quiet")
+
   expect_equal(loglevel(-19), DEBUG)
   expect_equal(loglevel(-1), DEBUG)
   expect_equal(loglevel(1), DEBUG)
@@ -17,16 +31,28 @@ test_that("The loglevel() constructor works as expected", {
   expect_error(loglevel("UNLOG"), "unknown logging level: UNLOG")
   expect_error(loglevel("ATA"), "unknown logging level: ATA")
   expect_error(loglevel("WAR"), "unknown logging level: WAR")
-  expect_error(loglevel(1:3), "atomic")
+  expect_error(loglevel(1:3), "must be a string")
   expect_error(loglevel(FALSE), "cannot determine")
 })
 
 test_that("Coercion works as expected", {
+  rlang::local_options(lifecycle_verbosity = "quiet")
+
   expect_equal(as.numeric(loglevel("DEBUG")), 1)
   expect_equal(as.character(loglevel("WARN")), "WARN")
   expect_equal(as.loglevel(loglevel("INFO")), loglevel("INFO"))
 })
 
-test_that("All log levels are available", {
-  expect_equal(unname(vapply(available.loglevels(), as.integer, integer(1))), 1:5)
+test_that("The verbosity() constructor creates equivalent log levels", {
+  rlang::local_options(lifecycle_verbosity = "quiet")
+
+  expect_equal(verbosity(-19), FATAL)
+  expect_equal(verbosity(-1), FATAL)
+  expect_equal(verbosity(1), FATAL)
+  expect_equal(verbosity(2), ERROR)
+  expect_equal(verbosity(3), WARN)
+  expect_equal(verbosity(4), INFO)
+  expect_equal(verbosity(5), DEBUG)
+  expect_equal(verbosity(6), DEBUG)
+  expect_equal(verbosity(60), DEBUG)
 })
